2f6a56c3078fb0d6d5d659b102a8b1d669744fac
Kon Akopov <konstantin.akopov@gmail.com>
2026-02-10 23:17:13 +1100

    feat: Add initial ShopSwipe landing page files

---
+++ script.js
@@ -0,0 +1,3 @@
+// This file can be used for any interactive JavaScript elements on your landing page.
+// For example, form validation, dynamic content loading, or smooth scrolling.
+// Currently, it's empty.
